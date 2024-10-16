1f15c927d1e88894b40743be8ca789233945d2b2
John Anthony Rico <johnrico364@gmail.com>
2024-10-16 09:00:07 +0800

    Delete unused files and update dependencies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import "./App.css";
+import "./app-component.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -10,5 +10,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-reportWebVitals();
