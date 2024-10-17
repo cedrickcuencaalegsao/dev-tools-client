8b793a8c4cf3e3f6c82e234300daa1c202a197ea
Cedrick C. Alegsao <cedrickcuencaalegsao@gmail.com>
2024-10-17 19:30:32 +0800

    suggestion first

---
+++ src/components/Suggestion.jsx
@@ -0,0 +1,8 @@
+export const Suggestions = () => {
+  return (
+    <div className="suggestion-container">
+      <span className="mr-5">Suggestions</span>
+      <button className="suggest-btn-active">Glassmorphism</button>
+    </div>
+  );
+};
