0e08bb6fc4a40b865251977a913fbe22789204e7
Cedrick C. Alegsao <cedrickcuencaalegsao@gmail.com>
2024-10-17 20:04:51 +0800

    fixing suggestion

--- src/components/Suggestion.jsx
+++ src/components/Suggestion.jsx
@@ -2,6 +2,11 @@ export const Suggestions = () => {
   return (
     <div className="p-4">
       <span className="text-lg font-semibold mb-4">Suggestions</span>
+      <div className="flex flex-wrap gap-3">
+        <button className="suggest-btn-active bg-blue-500 text-white px-4 py-2 rounded-lg shadow-lg">
+          Glassmorphism
+        </button>
+      </div>
     </div>
   );
 };
