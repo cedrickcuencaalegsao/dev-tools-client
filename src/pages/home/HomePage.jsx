2e79e6ecfc6302bf190b3aed6556fa396f738b9f
Cedrick C. Alegsao <cedrickcuencaalegsao@gmail.com>
2024-10-17 20:13:28 +0800

    fixing suggestion

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -8,36 +8,6 @@ export const HomePage = () => {
       <div className="lg:px-8 md:px-3">
         <Search />
         <Suggestions />
-        {/* <div className="wrapper search-container">
-          <IoSearch className="icon" />
-          <input
-            className="search-input"
-            type="text"
-            placeholder="Search Recipe here..."
-          />
-        </div> */}
-
-        {/* <div className="suggestion-container">
-          <span className="mr-5">Suggestions</span>
-
-          <button className="suggest-btn-active">Glassmorphism</button>
-          <button className="suggest-btn">Custom Borders</button>
-          <button className="suggest-btn">Tailwind Compo</button>
-          <button className="suggest-btn">Icons</button>
-          <button className="suggest-btn">Bootstrap Compo</button>
-          <button className="suggest-btn">Icon Components</button>
-          <button className="suggest-btn">Image Sliders</button>
-          <button className="suggest-btn">Scroll Animations</button>
-        </div> */}
-
-        {/* <div className="wrapper mt-3 pr-4 justify-end">
-          <select className="category-dropdown">
-            <option selected>Newest</option>
-            <option>Oldest</option>
-            <option>A-Z</option>
-            <option>Z-A</option>
-          </select>
-        </div> */}
 
         <div className="cards-container">
           <div className="flex justify-end">
