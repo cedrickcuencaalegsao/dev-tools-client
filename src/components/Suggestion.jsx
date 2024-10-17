79c24b995d653844a116b59fb12a3e98ca868204
Cedrick C. Alegsao <cedrickcuencaalegsao@gmail.com>
2024-10-17 19:55:33 +0800

    fixing suggestion

--- src/components/Suggestion.jsx
+++ src/components/Suggestion.jsx
@@ -1,10 +1,7 @@
 export const Suggestions = () => {
   return (
     <div className="p-4">
-      <div className="suggestion-container"> {/* No fixed height or scroll */}
-        <span className="text-lg font-semibold mb-4">Suggestions</span>
-        
-      </div>
+      <span className="text-lg font-semibold mb-4">Suggestions</span>
     </div>
   );
 };
